3dc3f1fe8e24a24dd4c92d08369a8683a677394f
DragonMoffon <happydog.aja@gmail.com>
2025-09-29 01:41:58 +1300

    add setup to digi's game pack

--- packs/digi/__init__.py
+++ packs/digi/__init__.py
@@ -1,5 +1,14 @@
 from .games import DigiGame
 
+from engine.pack import Pack
+
 __all__ = [
     "DigiGame"
 ]
+
+def setup():
+    return Pack(
+        games=(DigiGame, ),
+        transitions=(),
+        fails=()
+    )
