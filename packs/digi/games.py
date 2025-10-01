351e77795b668582d813d033c0860fba69231a9c
DigiDuncan <digiduncan@gmail.com>
2025-10-01 17:56:08 -0400

    new game

--- packs/digi/games.py
+++ packs/digi/games.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
+import random
 
 import arcade
+from arcade.types import AnchorPoint
 
 from aware.anim import bounce
 from engine.play import ContentFlag, PlayState, Game
@@ -65,31 +67,108 @@ class DoNothingGame(Game):
         if self.time > 3:
             self.fail()
 
-# Game that just stalls the app
-"""
-class EmulateStallGame(Game):
+VERY_LONG = 60 * 60
+BALL_COUNT = 10
+BALL_RADIUS = 20
+
+RED_SIDE_COLOR = noa.get_color(0, 8, 9)
+BLUE_SIDE_COLOR = noa.get_color(9, 8, 9)
+
+class SortGame(Game):
     def __init__(self, state: PlayState) -> None:
-        super().__init__(state, prompt = "STALL!", controls = "default.inputs.nothing", duration = 120.0)
-        ...
+        super().__init__(state, prompt = "SORT!", controls = "default.inputs.mouse", duration = VERY_LONG, flags = ContentFlag.COLORBLIND)
+        self.red_balls = [arcade.SpriteCircle(BALL_RADIUS, noa.get_color(0, 4, 9)) for _ in range(int(BALL_COUNT / 2))]
+        self.blue_balls = [arcade.SpriteCircle(BALL_RADIUS, noa.get_color(9, 4, 9))  for _ in range(int(BALL_COUNT / 2))]
+
+        self.spritelist = arcade.SpriteList()
+        self.spritelist.extend(self.red_balls + self.blue_balls)
+
+        self.red_side = self.window.rect.scale_axes((0.5, 1.0), AnchorPoint.CENTER_LEFT)
+        self.blue_side = self.window.rect.scale_axes((0.5, 1.0), AnchorPoint.CENTER_RIGHT)
+
+        self.cursor_pos = (0, 0)
+        self.selected_ball: arcade.SpriteCircle | None = None
+
+        self.scrambling_done = False
+        self.scrambling_text = arcade.Text('SCRAMBLING...', self.window.center_x, self.window.center_y, anchor_x = "center", anchor_y = "center", font_size = 72, font_name = "A-OTF Shin Go Pro", bold = True)
+
+        self.sorting_done = False
+        self.finish_time = float("inf")
+        self.sorting_text = arcade.Text('GOOD JOB!', self.window.center_x, self.window.center_y, anchor_x = "center", anchor_y = "center", font_size = 72, font_name = "A-OTF Shin Go Pro", bold = True)
+
+    @property
+    def all_balls(self) -> list[arcade.SpriteCircle]:
+        return self.red_balls + self.blue_balls
+    
+    def scramble(self) -> None:
+        for ball in self.all_balls:
+            x = random.randrange(BALL_RADIUS, self.window.width - BALL_RADIUS)
+            y = random.randrange(BALL_RADIUS, self.window.height - BALL_RADIUS - 65)
+            ball.position = (x, y)
+
+    @property
+    def completed(self) -> bool:
+        return (all([ball.position in self.red_side for ball in self.red_balls]) and
+                all([ball.position in self.blue_side for ball in self.blue_balls]))
+
+    def check_scramble(self) -> bool:
+        if self.completed or self.time < 1:
+            return False
+        for ball in self.all_balls:
+            other_balls = [b for b in self.all_balls if b is not ball]
+            for other_ball in other_balls:
+                if ball.collides_with_sprite(other_ball):
+                    return False
+        return True
     
     def start(self):
-        ...
+        self.scrambling_done = False
+        self.sorting_done = False
 
     def finish(self):
         ...
 
     def draw(self):
-        ...
+        arcade.draw_rect_filled(self.red_side, RED_SIDE_COLOR)
+        arcade.draw_rect_filled(self.blue_side, BLUE_SIDE_COLOR)
+        self.spritelist.draw()
+
+        if not self.scrambling_done:
+            self.scrambling_text.draw()
+
+        if self.sorting_done:
+            self.sorting_text.draw()
 
     def update(self, delta_time: float):
-        ...
+        if not self.scrambling_done:
+            if self.check_scramble():
+                self.scrambling_done = True
+                # This accesses a private member to do this, but does it have to be private?
+                self._duration = 6.0 + self.time
+                return
+            else:
+                self.scramble()
+        else:
+            if self.selected_ball:
+                self.selected_ball.position = self.cursor_pos
+            if self.completed:
+                self.sorting_done = True
+        if self.finish_time + 1 < self.time:
+            self.succeed()
 
     def on_time_runout(self):
-        self.succeed()
-
-    def on_input(self, symbol: int, modifier: int, pressed: bool):
-        ...
+        if self.sorting_done:
+            self.succeed()
+        else:
+            self.fail()
 
     def on_cursor_motion(self, x: float, y: float, dx: float, dy: float):
-        ...
-"""
+        self.cursor_pos = (x, y)
+
+    def on_input(self, symbol: int, modifier: int, pressed: bool):
+        if symbol == arcade.MOUSE_BUTTON_LEFT and pressed and not self.sorting_done:
+            for ball in self.all_balls:
+                if ball.collides_with_point(self.cursor_pos):
+                    self.selected_ball = ball
+        elif symbol == arcade.MOUSE_BUTTON_LEFT and not pressed:
+            self.selected_ball = None
